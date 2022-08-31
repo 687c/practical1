96209d449248f8c161419363a815869bd54f4161
Jimii <jimmyimpulse2@gmail.com>
2022-08-31 17:52:13 +0300

    added required code for implementations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
 }
+
+//the structs/classes
+struct Account {}
+
+struct Block {}
+
+struct Blockchain {}
+
+struct Hash {}
+
+struct KeyPair {}
+
+struct Operation {}
+
+struct Transaction {}
+
+struct Signature {}

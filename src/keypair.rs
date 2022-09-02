064a913746e448ba209cc441d8955dd78579bf10
Jimii <jimmyimpulse2@gmail.com>
2022-09-02 15:22:17 +0300

    feat: writing private key to keys file

--- src/keypair.rs
+++ src/keypair.rs
@@ -1,5 +1,5 @@
 use anyhow::{Ok, Result};
-use secp256k1::{rand::rngs::OsRng, PublicKey, SecretKey};
+use secp256k1::{rand::rngs::OsRng, Error, PublicKey, Secp256k1, SecretKey};
 use std::{
     fs::File,
     io::{Read, Write},
@@ -36,25 +36,21 @@ impl KeyPair {
     }
 
     pub fn save_key_to_file(key_pair: &Self /* path: Path */) -> Result<()> {
-        let private_key = key_pair.private_key.display_secret().to_string();
-        let public_key = key_pair.public_key.to_owned().to_string();
+        let private_key = key_pair.private_key.secret_bytes();
+        let formatted_private = format!("private key -> {:?}\n", private_key);
 
-        let formatted_public = format!("public key -> {public_key}\n");
-        let formatted_private = format!("private key -> {private_key}\n");
-
-        //Writing the keys to a file
+        //Writing the private key to the file
         let path = Path::new("keys.txt");
         /* TODO
          * GIVE USER ABILITY TO SPECIFY WHERE HE WANTS HIS KEYS TO BE SAVED
          */
         let mut file = File::create(path).unwrap();
-        file.write(formatted_public.as_bytes()).unwrap();
         file.write(formatted_private.as_bytes()).unwrap();
 
         Ok(())
     }
 
-    pub fn read_from_file(/* path: Path */) -> Result<(String, String)> {
+    pub fn read_from_file(/* path: Path */) -> Result<Self> {
         let path = Path::new("keys.txt");
         let mut file = File::open(path).unwrap();
 
@@ -64,18 +60,33 @@ impl KeyPair {
         let lines = contents.lines(); // let line1 = lines.next().unwrap();
 
         let mut private_key = String::new();
-        let mut public_key = String::new();
 
         for line in lines {
-            if line.contains("private key") {
-                let mut private_line = line.split("->");
-                private_key = private_line.nth(1).unwrap().trim().to_string();
-            } else {
-                let mut public_line = line.split("->");
-                public_key = public_line.nth(1).unwrap().trim().to_string();
+            //if field private key not found panic and stop 
+            if !line.contains("private key") {
+                panic!("private key not found");
             }
+
+            let mut private_line = line.split("->");
+            private_key = private_line.nth(1).unwrap().trim().to_string();
         }
 
-        Ok((private_key, public_key))
+        private_key.pop();
+        private_key.remove(0);
+
+        let data: Vec<u8> = private_key
+            .split(",")
+            .map(|val| val.trim().parse().unwrap())
+            .collect();
+
+        let private_key = SecretKey::from_slice(&data).unwrap();
+
+        let secp = Secp256k1::new();
+        let public_key = PublicKey::from_secret_key(&secp, &private_key);
+
+        Ok(Self {
+            private_key,
+            public_key,
+        })
     }
 }

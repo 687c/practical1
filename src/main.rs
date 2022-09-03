9f3cf82d93bd509dcfb6786edf82268534f5ee3f
Jimii <jimmyimpulse2@gmail.com>
2022-09-03 12:24:30 +0300

    WIP: main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
 mod keypair;
+mod signature;
+
+use std::path::Path;
 
 use keypair::KeyPair;
+use signature::Signature;
 
 fn main() {
-    let key_pair = KeyPair::new_key_pair();
-    
-    KeyPair::print_key_pair(key_pair);
+    //check if user has already generated keys and return
+    //ro generate them if he hasn't
+    let path = Path::new("keys.txt");
+    if path.exists() && path.metadata().unwrap().len() > 0 {
+        let retrieved_keys = KeyPair::read_from_file().unwrap();
+
+        // KeyPair::print_key_pair(&retrieved_keys);
+
+        // let Signature { signature } =
+        //     Signature::sign_data(&retrieved_keys.private_key, "hello world".to_string()).unwrap();
+        let signed = Signature::sign_data(&retrieved_keys.private_key, "hello world".to_string()).unwrap();
+        // println!("the signature {:?}", signed);
+        Signature::print_sig(&signed);
+        let verified = Signature::verify_sig(signed, retrieved_keys.public_key, "hello world".to_string()).unwrap();
+        println!("the verified {:?}", verified);
+
+        return;
+    }
+
+    let key_pair = KeyPair::new_key_pair().unwrap();
+    KeyPair::print_key_pair(&key_pair);
     // println!("the res {:?}", res);
+
+    KeyPair::save_key_to_file(&key_pair).unwrap();
 }
 
 //the structs/classes
@@ -23,8 +47,6 @@ struct Hash {}
 struct Operation {}
 
 struct Transaction {}
-
-struct Signature {}
 
 impl Account {
     pub fn new() -> String {
